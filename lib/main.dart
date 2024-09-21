f5abb9b6ecc9a2bf0c1d8269126eb85c560bba63
Mohamed Emad <mohamed.emad45621@gmail.com>
2024-09-22 00:28:04 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MuslimApp());
+}
+
+class MuslimApp extends StatelessWidget {
+  const MuslimApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Muslim app',
+      
+    );
+  }
+}
+
